4a605984999971c70f6225f5156dfcc98ec9feaf
whoisabbie <44428844+whoisabbie@users.noreply.github.com>
2020-08-22 21:31:34 +0530

    Completed first two sections for the site

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.hamburger-menu').on('click', function () {
+    $('.toggle').toggleClass('open');
+  });
+})
+
+$(function () {
+  $('.hamburger-menu').on('click', function () {
+    $('.nav-list').toggleClass('open');
+  });
+})
