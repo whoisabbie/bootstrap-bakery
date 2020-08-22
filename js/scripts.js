b12768a65a4feb6a4e7d4db46aaa74a2b98b015d
whoisabbie <44428844+abhiesheik@users.noreply.github.com>
2020-08-23 01:34:11 +0530

    Completed website
    - Added chef section and footer
    - Added query for the xl screens
    - Added animations for the complete site

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(function () {
-  $('.hamburger-menu').on('click', function () {
-    $('.toggle').toggleClass('open');
+  $(".hamburger-menu").on("click", function () {
+    $(".toggle").toggleClass("open");
+    $(".nav-list").toggleClass("open");
   });
-})
 
-$(function () {
-  $('.hamburger-menu').on('click', function () {
-    $('.nav-list').toggleClass('open');
+  $(".nav-list a").on("click", function () {
+    $(".toggle").toggleClass("open");
+    $(".nav-list").removeClass("open");
   });
-})
+
+  AOS.init({
+    easing: "ease",
+    duration: 1000
+  });
+});
+
+// $(function () {
+//   $('.hamburger-menu').on('click', function () {
+//     $('.nav-list').toggleClass('open');
+//   });
+// })
